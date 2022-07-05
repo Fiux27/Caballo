7091af435af79df8d0ab98c79f9b9110b00e2bda
Fiux27 <106567811+Fiux27@users.noreply.github.com>
2022-07-04 21:58:32 -0600

    Add files via upload

---
+++ Caballo/ProblemaCaballo.java
@@ -0,0 +1,10 @@
+public class ProblemaCaballo {
+    public static void main(String[] args) 
+    {
+        Caballo recorrido=new Caballo();
+        recorrido.recorre(0, 0, 1);
+        recorrido.imprimir();
+    }
+    
+}
+
